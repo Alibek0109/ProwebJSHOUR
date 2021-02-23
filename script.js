c0b9c79f9258cdd967892091c375fe62a5a9904f
unknown <kidiraliev97@inbox.ru>
2021-02-22 23:44:09 -0800

    first

---
+++ script.js
@@ -0,0 +1,84 @@
+var clock = +prompt('Введите время')
+
+
+switch (clock) {
+    case 1:
+        alert('Сейчас час ночи')
+        break;
+        case 2:
+            alert('Сейчас два часа ночи')
+            break;
+            case 3:
+                alert('Сейчас три часа ночи')
+                break;
+                case 4:
+                    alert('Сейчас четыре часа ночи')
+                    break;
+                    case 5:
+                        alert('Сейчас пять часов утра')
+                        break;
+                        case 6:
+                            alert('Сейчас шесть часов утра')
+                            break;
+                            case 7:
+                                alert('Сейчас семь часов утра')
+                                break;
+                                case 8:
+                                    alert('Сейчас восемь часов утра')
+                                    break;
+                                    case 9:
+                                        alert('Сейчас девять часов утра')
+                                        break;
+                                        case 10:
+                                            alert('Сейчас десять часов утра')
+                                            break;
+                                            case 11:
+                                                alert('Сейчас одиннадцать часов утра')
+                                                break;
+                                                case 12:
+                                                    alert('Сейчас двенадцать часов дня')
+                                                    break;
+                                                    case 13:
+                                                        alert('Сейчас час дня')
+                                                        break;
+                                                        case 14:
+                                                            alert('Сейчас два часа дня')
+                                                            break;
+                                                            case 15:
+                                                                alert('Сейчас три часа дня')
+                                                                break;
+                                                                case 16:
+                                                                    alert('Сейчас четыре часа дня')
+                                                                    break;
+                                                                    case 17:
+                                                                        alert('Сейчас пять часов вечера')
+                                                                        break;
+                                                                        case 18:
+                                                                            alert('Сейчас шесть часов вечера')
+                                                                            break;
+                                                                            case 19:
+                                                                                alert('Сейчас семь часов вечера')
+                                                                                break;
+                                                                                case 20:
+                                                                                    alert('Сейчас восемь часов вечера')
+                                                                                    break;
+                                                                                    case 21:
+                                                                                        alert('Сейчас девять часов вечера')
+                                                                                        break;
+                                                                                        case 22:
+                                                                                            alert('Сейчас десять часов ночи')
+                                                                                            break;
+                                                                                            case 23:
+                                                                                                alert('Сейчас одиннадцать часов ночи')
+                                                                                                break;
+                                                                                                case 24:
+                                                                                                    alert('Сейчас двеннадцать часов ночи')
+                                                                                                    break;
+                                                                                                    
+
+
+
+    default:
+        alert('Error')
+        break;
+}
